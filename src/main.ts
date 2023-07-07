d96369a9b9f7d2129e1225da85476eaa833f02fc
mamamamamaa <maks.denishchuk@gmail.com>
2023-07-07 14:25:47 +0300

    fix up

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = await app.get(ConfigService);
   const port = (await config.get<number>('APP_PORT')) || 7777;
