19fa7dc6ef8f070a6cb7cc33790b23595089c851
mamamama <maks.denishchuk@gmail.com>
2023-07-10 18:59:27 +0300

    fix up

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -9,6 +9,7 @@ export const databaseConfig: TypeOrmModuleAsyncOptions = {
     password: configService.get<string>('DB_PASSWORD'),
     username: configService.get<string>('DB_USERNAME'),
     database: configService.get<string>('DB_NAME'),
+    synchronize: true,
     autoLoadEntities: true,
     retryAttempts: 30,
     retryDelay: 10000,
